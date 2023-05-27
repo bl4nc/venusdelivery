fffd5d68b48e6ce2e30b9f92da55afa19fa43685
Rafael Araújo Rocha <40602200+bl4nc@users.noreply.github.com>
2023-05-27 14:43:15 -0300

    Frist

---
+++ src/app/components/Whatsapp/Whatsapp.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import "./Whatsapp.css"
+import { BsWhatsapp } from "react-icons/bs";
+
+export interface WhatsApp {
+  url: string;
+}
+
+export default function Whatsapp(props: WhatsApp) {
+  return (
+    <div className="flex relative z-50 pulsaDelay btn-whatsapp">
+      <a href="" target="_blank">
+        <div className="bg-[#25d366] p-4 rounded-full  text-white">
+          <BsWhatsapp className=''/>
+          {/* <i class="fa fa-whatsapp"></i> */}
+        </div>
+      </a>
+    </div>
+  )
+}
