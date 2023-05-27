e0c303691b836d41d500de03fe71bf2ecea982ff
Victor Wallace <111892682+Victor-Wallace@users.noreply.github.com>
2023-05-27 16:28:32 -0300

    criar conta

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -19,7 +19,15 @@ export default function register() {
         </div>
         <div className="mb-3 w-full flex flex-col p-2">
           <label className="text-sm text-navy-700 font-bold">Nome:</label>
-          <input type="text" id="nome" placeholder="Fulado" className="mt-2 h-12 items-center justify-center rounded-xl border bg-white p-3 text-sm outline-none border-gray-200" />
+          <input type="text" id="nome" placeholder="Digite seu nome" className="mt-2 h-12 items-center justify-center rounded-xl border bg-white p-3 text-sm outline-none border-gray-200" />
+        </div>
+        <div className="mb-3 w-full flex flex-col p-2">
+          <label className="text-sm text-navy-700 font-bold">Idade:</label>
+          <input type="text" id="nome" placeholder="Digite a sua idade" className="mt-2 h-12 items-center justify-center rounded-xl border bg-white p-3 text-sm outline-none border-gray-200" />
+        </div>
+        <div className="mb-3 w-full flex flex-col p-2">
+          <label className="text-sm text-navy-700 font-bold">Senha:</label>
+          <input type="password" id="nome" placeholder="Digite a sua senha" className="mt-2 h-12 items-center justify-center rounded-xl border bg-white p-3 text-sm outline-none border-gray-200" />
         </div>
         {/* <div className="mb-3">
           <label className="text-sm text-navy-700 font-bold">Nome:</label>
